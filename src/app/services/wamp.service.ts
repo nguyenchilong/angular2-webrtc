a4956b2f82b6d635af89de54cc133077f40b7969
Florian Moessle <Flo@Florians-MacBook-Pro.local>
2016-10-27 22:40:52 +0200

    added autobahnjs

--- src/app/services/wamp.service.ts
+++ src/app/services/wamp.service.ts
@@ -4,16 +4,32 @@ import { Injectable } from '@angular/core';
 
 export class WampService {
     constructor() {
-
+        /*
         let options: autobahn.IConnectionOptions = {
-            url: 'wss://demo.crossbar.io/ws', realm: 'realm1'
+            url: 'ws://10.90.79.146:8084/'
         };
 
         let connection = new autobahn.Connection(options);
         connection.onopen = (session) => {
             console.log(session);
-            session.subscribe('com.myapp.hello', () => { console.log('bla') });
+            session.subscribe('signaling/', () => { console.log('bla') });
         };
         connection.open();
+        */
+
+        let conn = new ab.Session('wss://192.168.43.109',
+        // onsuc:
+        function() {
+            conn.subscribe('signaling/', function(topic, data) {
+                console.log(data);
+            });
+        },
+        // on err
+        function() {
+        },
+        {'skipSubprotocolCheck': true}
+
+    );
+
     }
 }
