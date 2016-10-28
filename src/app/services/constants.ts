78455f0afc266cf64d43e23156588a4a5ec224f7
Florian Moessle <Flo@Florians-MacBook-Pro.local>
2016-10-28 10:22:06 +0200

    Added const for WAMP

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -1,3 +1,4 @@
 export const MOBILE = (typeof window !== 'undefined') ? (window.screen.availWidth < 800) : true;
 export const API_BASE_URL: string = `http://${HOST}:${PORT}`;
 export const SOCKET: string = 'https://chor-am-killesberg.de:1234';
+export const WAMP: string = 'ws://10.90.16.221:8084';
--- src/app/services/wamp.service.ts
+++ src/app/services/wamp.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { WAMP } from './constants';
 
 @Injectable()
 
 export class WampService {
     constructor() {
 
-        let conn = new ab.Session('wss://192.168.43.109',
+        let conn = new ab.Session(
+        // URL of socket
+        WAMP,
         // onsuc:
         function() {
             conn.subscribe('signaling/', function(topic, data) {
