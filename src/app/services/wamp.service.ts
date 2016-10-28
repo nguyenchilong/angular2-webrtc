d4aa0c930a60c120ee2083f80337803d748581f5
Florian Moessle <Flo@Florians-MacBook-Pro.local>
2016-10-28 10:07:06 +0200

    clean up

--- src/app/services/wamp.service.ts
+++ src/app/services/wamp.service.ts
@@ -4,18 +4,6 @@ import { Injectable } from '@angular/core';
 
 export class WampService {
     constructor() {
-        /*
-        let options: autobahn.IConnectionOptions = {
-            url: 'ws://10.90.79.146:8084/'
-        };
-
-        let connection = new autobahn.Connection(options);
-        connection.onopen = (session) => {
-            console.log(session);
-            session.subscribe('signaling/', () => { console.log('bla') });
-        };
-        connection.open();
-        */
 
         let conn = new ab.Session('wss://192.168.43.109',
         // onsuc:
