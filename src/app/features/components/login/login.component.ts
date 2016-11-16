f40f49e2c1786de8e4ac5647acd024e50356f4b3
Florian Moessle <Flo@Florians-MacBook-Pro.local>
2016-11-16 10:20:38 +0100

    new login form

--- src/app/features/components/login/login.component.ts
+++ src/app/features/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ChangeDetectionStrategy } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'login-component',
@@ -13,17 +14,23 @@ export class LoginComponent {
 
     @ViewChild('username') username;
     @ViewChild('username') password;
+    loginform: FormGroup;
 
     constructor(
         private authservice: AuthService,
-        private router: Router) {
+        private router: Router,
+        private formBuilder: FormBuilder) {
         if (this.authservice.isAuthorized) {
             this.router.navigate(['']);
         }
+        this.loginform = this.formBuilder.group({
+            email: '',
+            password: ''
+        });
     }
 
     login(): void {
-        this.authservice.setJWT(this.username._value);
+        this.authservice.setJWT(this.loginform.value.email);
         this.router.navigate(['']);
     }
 }
