39299e9b27fd07ad4aa8ff2270715638348736f6
Florian Moessle <Flo@Florians-MBP.fritz.box>
2016-11-20 19:13:34 +0100

    spinner for login-component

--- src/app/features/components/login/login.component.ts
+++ src/app/features/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
@@ -13,28 +13,39 @@ import { RestService } from '../../../services/rest.service';
 
 export class LoginComponent {
 
-    @ViewChild('username') username;
-    @ViewChild('username') password;
     loginform: FormGroup;
+    isloading: boolean = false;
 
     constructor(
         private authservice: AuthService,
         private router: Router,
         private formBuilder: FormBuilder,
-        private restservice: RestService) {
+        private restservice: RestService,
+        private changeDetectionRef: ChangeDetectorRef) {
         if (this.authservice.isAuthorized) {
             this.router.navigate(['']);
         }
         this.loginform = this.formBuilder.group({
-            email: '',
+            username: '',
             password: ''
         });
     }
 
     login(): void {
-        this.restservice.authorizeUser(this.loginform.value.email, this.loginform.value.password)
-        .subscribe(data => console.log(data._body.token));
+        this.isloading = true;
+        this.restservice.authorizeUser(this.loginform.value.username, this.loginform.value.password)
+            .subscribe(
+            (data) => {
+                this.isloading = false;
+                this.changeDetectionRef.markForCheck();
+            },
+            (err) => {
+                this.isloading = false;
+                this.changeDetectionRef.markForCheck();
+            }
+            );
         this.authservice.setJWT(this.loginform.value.email);
-        this.router.navigate(['']);
+        // this.router.navigate(['']);
+        this.loginform.controls['password'].setValue('');
     }
 }
