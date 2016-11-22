291d51f170f59feae2240755d4d7c874640df15a
Florian Moessle <Flo@Florians-MBP.fritz.box>
2016-11-22 11:41:40 +0100

    added videomenu with hovereffects

--- src/app/features/components/video/video.component.ts
+++ src/app/features/components/video/video.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ViewChild, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'video-component',
@@ -8,6 +8,28 @@ import { Component, ViewChild, ChangeDetectionStrategy } from '@angular/core';
 })
 
 export class VideoComponent {
+    @Output() toggleAudio =  new EventEmitter();
+    @Output() toggleVideo = new EventEmitter();
     @ViewChild('myVideo') myVideo;
     @ViewChild('otherVideo') otherVideo;
+    showmenu: boolean = false;
+    videoactive: boolean = true;
+    audioactive: boolean = true;
+
+    enter(): void {
+        this.showmenu = true;
+    }
+    leave(): void {
+        this.showmenu = false;
+    }
+
+    audioClicked(): void {
+        this.toggleAudio.emit();
+        this.audioactive = !this.audioactive;
+    }
+
+    videoClicked(): void {
+        this.toggleVideo.emit();
+        this.videoactive = !this.videoactive;
+    }
 }
