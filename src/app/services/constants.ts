ad85c8fe3ca48ca1a0b068c1d8ae00a269fc5c8c
Florian Moessle <Flo@Florians-MacBook-Pro.local>
2016-11-30 13:04:27 +0100

    enbaled https in rest and socket

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -1,5 +1,5 @@
 export const MOBILE = (typeof window !== 'undefined') ? (window.screen.availWidth < 800) : true;
 export const API_BASE_URL: string = `http://${HOST}:${PORT}`;
 export const SOCKET: string = 'https://chor-am-killesberg.de:1234';
-export const REST: string = 'http://chor-am-killesberg.de:8001';
-export const WAMP: string = 'ws://chor-am-killesberg.de:8001';
+export const REST: string = 'https://chor-am-killesberg.de:8001';
+export const WAMP: string = 'wss://chor-am-killesberg.de:8001';
