68d2240920c8a3229ff3316d78a2eb5da9b26256
Florian Moessle <Flo@Florians-MBP.fritz.box>
2016-12-25 19:17:28 +0100

    added MdSnackBarRef and redirecting to 'caller'

--- src/app/services/call.service.ts
+++ src/app/services/call.service.ts
@@ -1,23 +1,30 @@
 import { Injectable } from '@angular/core';
 import { WampService } from './wamp.service';
-import { MdSnackBar } from '@angular/material';
+import { MdSnackBar, MdSnackBarRef } from '@angular/material';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable()
 
 export class CallService {
     callsubscription: Subscription = new Subscription();
     usertocallid: number;
+    snackbarref: MdSnackBarRef<any>;
 
     constructor(
         private wampservice: WampService,
-        public snackBar: MdSnackBar
+        private snackBar: MdSnackBar,
+        private router: Router
     ) {
     };
 
     subscribeCall(): void {
         this.callsubscription = this.wampservice.callObservable.subscribe(call => {
-            this.snackBar.open(call.title + ' ' + call.lastname + ' ' +  'is now available', 'Start');
+            this.snackbarref = this.snackBar.open(call.title + ' ' + call.lastname + ' ' + 'is now available', 'Start');
+            this.snackbarref.onAction().subscribe(() => {
+                this.router.navigate(['caller']);
+                //TODO: start calling prof
+            });
         });
     }
 
