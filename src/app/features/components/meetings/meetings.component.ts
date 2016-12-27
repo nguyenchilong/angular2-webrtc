137033d78a53a85c39e40d56752741bfc7dd8a77
Florian Moessle <Flo@Florians-MBP.fritz.box>
2016-12-27 19:20:49 +0100

    test creation of meeting

--- src/app/features/components/meetings/meetings.component.ts
+++ src/app/features/components/meetings/meetings.component.ts
@@ -5,7 +5,10 @@ import { MdDialogRef, MdDialog, MdDialogConfig } from '@angular/material/dialog'
 import { MeetingDialog } from '../../dialogs/meeting-dialog/meeting-dialog.component';
 import { Slot } from '../../../model/slot';
 import { UserLogin } from '../../../model/user-login';
+// Test imports, remove when done
 import { WampService } from '../../../services/wamp.service';
+import { RestService } from '../../../services/rest.service';
+import { MeetingProfessor } from '../../../model/meeting-professor';
 
 @Component({
     selector: 'meetings-component',
@@ -21,7 +24,8 @@ export class MeetingsCompontent {
     constructor(
         private store: Store<any>,
         private dialog: MdDialog,
-        private wampservice: WampService
+        private wampservice: WampService,
+        private restservice: RestService
     ) {
         this.slots = this.store.select(store => store.slots);
         this.user = this.store.select(store => store.user);
@@ -44,7 +48,7 @@ export class MeetingsCompontent {
         // when closing dialog
         this.dialogRef.afterClosed().subscribe(result => { });
     }
-
+    //  SOME TEST:
     testCall(): void {
         this.wampservice.sendWithSocket(206, {
             type: 'call',
@@ -52,7 +56,20 @@ export class MeetingsCompontent {
             title: localStorage.getItem('user_title'),
             lastname: localStorage.getItem('user_lastname')
         })
-        .subscribe(data => {});
+            .subscribe(data => { });
+    }
+    createMeeting(): void {
+        let d1 = new Date();
+        let d2 = new Date();
+        d1.setDate(new Date().getDate());
+        d2.setDate(new Date().getDate() + 3);
+        let meeting: MeetingProfessor = {
+            slots: [],
+            startDate: d1,
+            endDate: d2,
+            status: 'active'
+        };
+        this.restservice.createMeeting(meeting);
     }
 
 }
