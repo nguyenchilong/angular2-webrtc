c414d403e182049a65c3a5ff894fef1efd423d03
Florian Moessle <Flo@Florians-MBP.fritz.box>
2017-01-02 21:38:23 +0100

    loggin

--- src/app/features/dialogs/meeting-dialog/meeting-dialog.component.ts
+++ src/app/features/dialogs/meeting-dialog/meeting-dialog.component.ts
@@ -35,6 +35,8 @@ export class MeetingDialog {
         this.store.select(store => store.professors).first().subscribe(first => {
             // fill selectedProfessor and selectedStudiecourse
             if (first !== []) {
+                console.log('log if there is a prod');
+                console.log(first);
                 this.selectedProfessor = first[0];
                 this.selectedStudiecourse = this.selectedProfessor.studycourses[0];
             } else {
