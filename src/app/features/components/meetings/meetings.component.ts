abb8d572bf3f669d7c34f9a99d34179f549e2cfc
Florian Moessle <Flo@Florians-MBP.fritz.box>
2017-01-02 21:07:38 +0100

    fix template meetings.component

--- src/app/features/components/meetings/meetings.component.ts
+++ src/app/features/components/meetings/meetings.component.ts
@@ -28,11 +28,6 @@ export class MeetingsCompontent {
         private restservice: RestService
     ) {
         this.slots = this.store.select(store => store.slots);
-        this.slots.subscribe((x) => {
-            console.log('slot in store:');
-            console.log(x);
-
-        });
         this.user = this.store.select(store => store.user);
     };
 
