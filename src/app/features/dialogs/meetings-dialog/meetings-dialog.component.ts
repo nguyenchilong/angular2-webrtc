d73679f40e45dd33d746bc8f51070c1cd28fb959
Florian Moessle <Flo@Florians-MacBook-Pro.local>
2017-01-04 20:49:31 +0100

    subscrie to user

--- src/app/features/dialogs/meetings-dialog/meetings-dialog.component.ts
+++ src/app/features/dialogs/meetings-dialog/meetings-dialog.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { MdDialogRef, MdDialog, MdDialogConfig } from '@angular/material/dialog';
 import { MeetingDialog } from '../../dialogs/meeting-dialog/meeting-dialog.component';
 import { Slot } from '../../../model/slot';
+import { UserLogin } from '../../../model/user-login';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 @Component({
     selector: 'meetings-dialog',
@@ -12,10 +15,14 @@ export class MeetingsDialog {
 
     slots: Array<Slot>; // set externally by CalendarComponent.openDialog()
     dialogRef2: MdDialogRef<MeetingDialog>;
+    user: Observable<UserLogin>;
+
 
     constructor(
-            public dialogRef: MdDialogRef<MeetingsDialog>,
-            public dialog: MdDialog) {
+        public dialogRef: MdDialogRef<MeetingsDialog>,
+        public dialog: MdDialog,
+        private store: Store<any>) {
+        this.user = this.store.select(store => store.user);
     }
 
     openDialog(slot: Slot) {
@@ -23,7 +30,7 @@ export class MeetingsDialog {
         this.dialogRef2 = this.dialog.open(MeetingDialog, config);
         this.dialogRef2.componentInstance.slot = slot;
         // when closing dialog
-        this.dialogRef2.afterClosed().subscribe(result => {});
+        this.dialogRef2.afterClosed().subscribe(result => { });
         this.dialogRef.close();
     }
 
