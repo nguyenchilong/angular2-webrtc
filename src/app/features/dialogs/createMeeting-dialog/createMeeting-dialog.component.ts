04cf960ee89fefeb9dfa52aa455cb5bedbb3a2b2
Florian Moessle <fmoessle@yahoo.de>
2017-01-17 16:35:01 +0100

    test date and time

--- src/app/features/dialogs/createMeeting-dialog/createMeeting-dialog.component.ts
+++ src/app/features/dialogs/createMeeting-dialog/createMeeting-dialog.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MdDialogRef } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
 
 @Component({
     selector: 'createMeeting-component',
@@ -8,8 +10,18 @@ import { MdDialogRef } from '@angular/material/dialog';
 
 export class CreateMeetingComponent {
 
+    meetingGroup: FormGroup;
 
-    constructor(public dialogRef: MdDialogRef<CreateMeetingComponent>) {}
+    constructor(
+        public dialogRef: MdDialogRef<CreateMeetingComponent>,
+        public formBuilder: FormBuilder) {
+            let today = moment(new Date()).format('YYYY-MM-DD');
+        this.meetingGroup = this.formBuilder.group({
+            day: today,
+            start: '10:00',
+            end: '11:30',
+        });
+    }
 
     create(): void {
         this.dialogRef.close();
