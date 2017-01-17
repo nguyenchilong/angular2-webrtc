6970ad9669ca276025cd0f97814d790e58a4551a
Florian Moessle <fmoessle@yahoo.de>
2017-01-17 16:52:23 +0100

    fix

--- src/app/features/dialogs/createMeeting-dialog/createMeeting-dialog.component.ts
+++ src/app/features/dialogs/createMeeting-dialog/createMeeting-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MdDialogRef } from '@angular/material/dialog';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as moment from 'moment';
+import { RestService } from '../../../services/rest.service';
 
 @Component({
     selector: 'createMeeting-component',
@@ -14,12 +15,13 @@ export class CreateMeetingComponent {
 
     constructor(
         public dialogRef: MdDialogRef<CreateMeetingComponent>,
-        public formBuilder: FormBuilder) {
+        public formBuilder: FormBuilder,
+        public rest: RestService) {
         let today = moment(new Date()).format('YYYY' + '-' + 'MM' + '-' + 'DD');
         this.meetingGroup = this.formBuilder.group({
-            day: today,
-            start: '10:00',
-            end: '11:30',
+            day: [today, Validators.required],
+            start: ['10:00', Validators.required],
+            end: ['11:30', Validators.required]
         });
     }
 
